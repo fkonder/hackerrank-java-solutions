ff98d01ee59afa99f6ced0f5c0798d22de53e83a
fkonder <omerfaruk0k0@gmail.com>
2022-07-18 13:19:38 +0300

    static initialize problem solved

---
+++ 10-static-initialize.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+static boolean flag = true;
+static int H, B, area;
+
+static{
+    Scanner scan = new Scanner(System.in);
+    B = scan.nextInt();
+    scan.nextLine();
+    H = scan.nextInt();
+    scan.close();
+    if(B<=0 || H<=0){
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+        flag = false;
+}
+}
+
+
+
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
